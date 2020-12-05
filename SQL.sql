b4c4e71094a3ddab151001281b0d76f9c86bc7a3
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2020-12-05 10:41:18 -0700

    sql_starter

---
+++ SQL.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Country (
+  id INT PRIMARY KEY,
+  Country TEXT,
+  Country_Code TEXT
+);
+
+CREATE TABLE Education (
+  id INT PRIMARY KEY,
+  Country TEXT,
+  Country_Code TEXT,
+  Year INT
+);
+CREATE TABLE Happiness (
+  id INT PRIMARY KEY,
+  Country TEXT,
+  Country_Code TEXT,
+  Year INT
+);
+
+CREATE TABLE Wikipedia (
+  id INT PRIMARY KEY,
+   Country TEXT
+);
